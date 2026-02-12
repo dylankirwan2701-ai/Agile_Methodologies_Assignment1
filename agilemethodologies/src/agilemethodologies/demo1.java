6d3cc88761d39c1e0e637d2809c116daa35b5290
A00330611 <A00330611@GOV-8FJJ164>
2026-02-12 10:03:33 +0000

    Test

---
+++ agilemethodologies/src/agilemethodologies/demo1.java
@@ -0,0 +1,13 @@
+package agilemethodologies;
+
+public class demo1 {
+	
+	public int method1(int number) {
+
+		int square = number * number;
+		return square;
+
+	} // end method1
+
+}// end Demo1
+
